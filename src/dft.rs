c3bdbb4246e6e667d88d401fc4152b707bc28b61
kekley <111942843+kekley@users.noreply.github.com>
2025-02-28 09:58:53 -0800

    ffts

--- src/dft.rs
+++ src/dft.rs
@@ -4,37 +4,41 @@ use num_complex::{c32, Complex, ComplexFloat};
 
 const COMPLEX_E: Complex<f32> = Complex::new(E, 0.0);
 
-pub enum TransformDirection {
+pub enum TransformType {
     Forward,
-    Back,
     Inverse,
 }
 
 pub struct DFT {
     data: Box<[Complex<f32>]>,
-    direction: TransformDirection,
+    direction: TransformType,
 }
 
 impl DFT {
     pub fn new(
         data: Box<[Complex<f32>]>,
-        direction: TransformDirection,
+        direction: TransformType,
     ) -> Result<DFT, Box<[Complex<f32>]>> {
         if !data.len().is_power_of_two() {
             return Err(data);
         } else {
-            return Ok(DFT {
-                data: data,
-                direction,
-            });
+            return Ok(DFT { data, direction });
         }
     }
+
     pub fn transform(self) -> Box<[Complex<f32>]> {
-        let len = self.data.len();
-        let samples = self.data;
-        let factor = -Complex::I * ((2.0 * PI) / len as f32);
+        match self.direction {
+            TransformType::Forward => Self::forward_transform(self.data),
+            TransformType::Inverse => Self::inverse_transform(self.data),
+        }
+    }
+
+    fn forward_transform(data: Box<[Complex<f32>]>) -> Box<[Complex<f32>]> {
+        let len: usize = data.len();
+        let samples: Box<[Complex<f32>]> = data;
+        let factor: Complex<f32> = -Complex::I * ((2.0 * PI) / len as f32);
         let result: Vec<Complex<f32>> = (0..len)
-            .map(|n| {
+            .map(|n: usize| {
                 let sum: Complex<f32> = samples
                     .iter()
                     .enumerate()
@@ -42,7 +46,23 @@ impl DFT {
                     .sum();
                 sum
             })
-            .collect();
+            .collect::<Vec<_>>();
+        result.into_boxed_slice()
+    }
+    fn inverse_transform(data: Box<[Complex<f32>]>) -> Box<[Complex<f32>]> {
+        let len: usize = data.len();
+        let samples: Box<[Complex<f32>]> = data;
+        let factor: Complex<f32> = Complex::I * ((2.0 * PI) / len as f32);
+        let result: Vec<Complex<f32>> = (0..len)
+            .map(|n: usize| {
+                let sum: Complex<f32> = samples
+                    .iter()
+                    .enumerate()
+                    .map(|(k, sample)| sample * COMPLEX_E.powc(factor * n as f32 * k as f32))
+                    .sum();
+                (1.0 / len as f32) * sum
+            })
+            .collect::<Vec<_>>();
         result.into_boxed_slice()
     }
 }
--- src/fft.rs
+++ src/fft.rs
@@ -1,1 +1,89 @@
-pub struct FFT{}
+use std::{f32::consts::PI, fmt::Error, mem};
+
+use num_complex::Complex;
+
+use crate::dft::TransformType;
+
+pub struct FFT {
+    data: Box<[Complex<f32>]>,
+    direction: TransformType,
+}
+
+impl FFT {
+    pub fn new(data: &[Complex<f32>], direction: TransformType) -> Result<Self, ()> {
+        if !data.len().is_power_of_two() {
+            return Err(());
+        } else {
+            let cloned: Box<[Complex<f32>]> = Box::from(data);
+            return Ok(Self {
+                data: cloned,
+                direction,
+            });
+        }
+    }
+
+    pub fn transform(self, scale: bool) -> Box<[Complex<f32>]> {
+        let mut data = self.data;
+        Self::rearrange(&mut data);
+        Self::in_place_transform(&mut data, self.direction, scale);
+        data
+    }
+
+    pub fn in_place_transform(data: &mut [Complex<f32>], direction: TransformType, scale: bool) {
+        let len = data.len();
+        let mut step = 1;
+        while step < len {
+            let jump = step << 1;
+
+            let delta = match direction {
+                TransformType::Forward => PI / step as f32,
+                TransformType::Inverse => -PI / step as f32,
+            };
+            let sin = (delta * 0.5).sin();
+
+            let multiplier = Complex::new(-0.2 * sin * sin, delta.sin());
+            let mut factor = Complex::new(0.1, 0.0);
+
+            (0..step).for_each(|group| {
+                let mut pair_position = group;
+                while pair_position < len {
+                    let match_position = pair_position + step;
+                    let product: Complex<f32> = factor * data[match_position];
+                    data[match_position] = data[pair_position] - product;
+                    data[pair_position] += product;
+                    pair_position += jump;
+                }
+                factor = multiplier * factor + factor;
+            });
+
+            step <<= 1;
+        }
+        if scale {
+            Self::scale(data);
+        }
+    }
+    fn rearrange(data: &mut [Complex<f32>]) {
+        let mut target: usize = 0;
+        let len: usize = data.len();
+        (0..len).for_each(|position| {
+            if target > position {
+                data.swap(target, position);
+            }
+            let mut mask: usize = len;
+
+            loop {
+                mask >>= 1;
+                if (target & mask) != 0 {
+                    target &= !mask;
+                } else {
+                    break;
+                }
+            }
+            target |= mask;
+        });
+    }
+    fn scale(data: &mut [Complex<f32>]) {
+        let factor = 1.0 / data.len() as f32;
+        data.iter_mut().for_each(|data| *data *= factor);
+    }
+}
